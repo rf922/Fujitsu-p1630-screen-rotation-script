4c924044feb9bcf6ecd879fb549352e6036bc27e
rf922 <silver@scylla.local>
2025-01-11 09:48:14 -0800

    Adjust output formatting

--- bin/rotate-screen-v2.sh
+++ bin/rotate-screen-v2.sh
@@ -19,7 +19,7 @@ normal_calibration=(
 "Option  \"InvertY\"  \"0\""
 )
 right_calibration=(
-#"Option  \"Calibration\"  \"340 3974 102 3908\""
+"Option  \"Calibration\"  \"340 3974 102 3908\""
 "Option  \"SwapXY\"  \"1\""
 "Option  \"InvertX\"  \"0\""
 "Option  \"InvertY\"  \"1\""
@@ -50,22 +50,22 @@ rotation="$(xrandr --query --verbose | grep 'LVDS-1' | cut --delimiter=' ' -f6)"
 case "${rotation}" in
   "normal")
     next_rotation="right"
-    new_calibration="${right_calibration[@]}"
+    new_calibration=("${right_calibration[@]}")
     transform_matrix="$right"
     ;;
   "right")
     next_rotation="inverted"
-    new_calibration="${inverted_calibration[@]}"
+    new_calibration=("${inverted_calibration[@]}")
     transform_matrix="$inverted"
     ;;
   "inverted")
     next_rotation="left"
-    new_calibration="${left_calibration[@]}"
+    new_calibration=("${left_calibration[@]}")
     transform_matrix="$left"
     ;;
   *)
     next_rotation="normal"
-    new_calibration="${normal_calibration[@]}"
+    new_calibration=("${normal_calibration[@]}")
     transform_matrix="$normal"
     ;;
 esac
@@ -75,13 +75,16 @@ xrandr --output LVDS-1 --rotate "${next_rotation}"
 
 # Update the calibration file
 {
-echo 'Section "InputClass"'
-echo '    Identifier  "calibration"'
-echo '    MatchProduct    "Fujitsu Component USB Touch Panel"'
-echo '    Driver  "evdev"'
-echo "$new_calibration"
-echo 'EndSection'
+    echo 'Section "InputClass"'
+    echo '    Identifier  "calibration"'
+    echo '    MatchProduct    "Fujitsu Component USB Touch Panel"'
+    echo '    Driver  "evdev"'
+    for option in "${new_calibration[@]}"; do
+        echo "    $option"
+    done
+    echo 'EndSection'
 } > /etc/X11/xorg.conf.d/99-calibration.conf
 
+# Set the transform matrix
 xinput set-prop "Fujitsu Component USB Touch Panel" "Coordinate Transformation Matrix" $transform_matrix
 
